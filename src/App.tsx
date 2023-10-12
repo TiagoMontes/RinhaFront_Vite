55376ea3384d47e3648154f2458932fd43d3e7b5
tiago <tiagosantos6303@gmail.com>
2023-10-12 14:13:37 -0500

    <feat>: creating a json viewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jsonContent, setJSONContent] = useState<boolean>();
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = (event: any) => {
+      try {
+        setJSONContent(JSON.parse(event.target.result));
+      } catch (error) {
+        console.error('Erro ao analisar o arquivo JSON', error);
+        setJSONContent(false);
+      }
+    };
+
+    reader.readAsText(file);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>JSON Tree Viewer</h1>
+        <p>Simple JSON Viewer that runs completely on-client. No data exchange </p>
+        <input type="file" onChange={handleFileChange} />
+        
+        {jsonContent && (
+          <div className='divbase'>
+            <h2>Conteúdo do arquivo JSON:</h2>
+              <pre>{JSON.stringify(jsonContent, null, 2)}</pre>
+          </div>
+        )}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
